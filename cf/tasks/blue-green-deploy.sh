4e921a664aa7171ebc6c5bad388d4375231011d6
forestoden6 <forestoden6@gmail.com>
2018-08-22 13:35:30 -0600

    Forcing delete of old deploy

--- cf/tasks/blue-green-deploy.sh
+++ cf/tasks/blue-green-deploy.sh
@@ -49,6 +49,6 @@ for name in $app_names; do
     then
       # TO DO: clean up blue
       cf unmap-route $name $CF_DOMAIN --hostname $CF_HOSTNAME
-      cf delete $name
+      cf delete -f $name
     fi
 done
